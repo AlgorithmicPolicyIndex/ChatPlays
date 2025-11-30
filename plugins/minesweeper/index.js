161773d2e7e8106cb4f3469aae971e36878154c6
API <60835424+AlgorithmicPolicyIndex@users.noreply.github.com>
2025-11-30 14:12:26 -0700

    Create index.js

---
+++ plugins/minesweeper/index.js
@@ -0,0 +1,45 @@
+﻿import {BrowserWindow} from "electron";
+
+function init({ Enable, MessageBus }) {
+    const window = BrowserWindow.getAllWindows().find(w => w.title === "API - MineSweeper");
+    if (!Enable) {
+        if (window) window.close();
+        return;
+    }
+    if (window) {
+        window.focus();
+        return;
+    }
+
+
+    // TEST
+    MessageBus.on("chat_message", ({channel, message}) => {
+       console.log("From Plugin", channel, message);
+    });
+    //
+
+    const game = new BrowserWindow({
+        width: 600,
+        height: 800,
+        show: false,
+        frame: false,
+        roundedCorners: false,
+        resizable: false,
+        maximizable: false,
+        title: "API - MineSweeper"
+    });
+
+    game.show();
+}
+
+export const info = {
+        author: "AlgorithmicPolicyIndex",
+        name: "Mine Sweeper",
+        dirName: "MineSweeper",
+        description: "Allows Chat to play Mine Sweeper. Can connect to OBS for visual fun!",
+        type: "external",
+        init: init,
+        options: {
+            configurable: false,
+        }
+};
